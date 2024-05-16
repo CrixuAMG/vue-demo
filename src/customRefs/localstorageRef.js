19a28535a356cbcf503bf53803e3fa2a8f05c6cf
ckaal <c.kaal@visymo.com>
2024-05-16 15:29:44 +0200

    Initial commit

---
+++ src/customRefs/localstorageRef.js
@@ -0,0 +1,43 @@
+import { customRef } from 'vue';
+
+const persist = (key, value) => {
+    let dataStoreValue = value;
+
+    if (typeof value === 'object' || Array.isArray(value)) {
+        dataStoreValue = JSON.stringify(value);
+    }
+
+    localStorage.setItem(key, dataStoreValue);
+
+    return value;
+};
+
+export default function useLocalstorageRef(key, value) {
+    return customRef((track, trigger) => {
+        return {
+            get() {
+                track();
+
+                let localStorageValue = localStorage.getItem(key);
+                if (localStorageValue) {
+                    if (localStorageValue === 'null') {
+                        localStorageValue = '';
+                    }
+
+                    try {
+                        return JSON.parse(localStorageValue);
+                    } catch (e) {
+                        return localStorageValue;
+                    }
+                }
+
+                return persist(key, value);
+            },
+            set(newValue) {
+                persist(key, newValue);
+
+                trigger();
+            }
+        };
+    });
+};
